b5471451861ab84c0a4e3c4fb05bfb0abe37d22c
jiangl <less.jiang@dell.com>
2023-04-10 08:04:12 +0800

    Create PipelineExecutionService.java

---
+++ block-service/src/main/java/org/teapotech/blockly/service/PipelineExecutionService.java
@@ -0,0 +1,9 @@
+package org.teapotech.blockly.service;
+
+import org.teapotech.blockly.config.PipelineExecutorProperties;
+
+public interface PipelineExecutionService {
+
+    PipelineExecutorProperties getPipelineExecutorProperties();
+
+}
